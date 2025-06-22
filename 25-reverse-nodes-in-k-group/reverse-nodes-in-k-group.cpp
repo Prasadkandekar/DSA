609257fb8463cc1f0933bfa0878367c40855a9cd
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2025-06-22 12:12:17 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (39.05%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,70 +1,53 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    // ListNode* reverseLL(ListNode* head){
-    //     if(head==nullptr || head->next==nullptr)return head;
-    //     ListNode* newHead = reverseLL(head->next);
-    //     ListNode* front = head->next;
-    //     front->next = head;
-    //     head = nullptr;
-    //     return newHead;
-    // }
-    ListNode* reverseLL(ListNode* head){
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        while(curr){
-            ListNode* nxt = curr->next;
-            curr->next = prev;
-            prev= curr;
-            curr = nxt;
-        }
-        return prev;
-    }
-    ListNode* findKthNode(ListNode* temp, int k){
-        k -= 1;
-        while(temp && k>0){
-            k--;
-            temp= temp->next;
-        }
-        return temp;
-    }
-    
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* temp= head, *nextNode, *prevNode = nullptr,*kthNode;
-        ListNode* newHead = nullptr;
 
-        while(temp!= nullptr)
-        {
-            kthNode = findKthNode(temp,k);
-            if(kthNode==NULL)
-            {
-                if(prevNode) prevNode->next = temp;
-                break;
-            }
 
-            nextNode = kthNode->next;
-            kthNode->next = nullptr;
-            ListNode* reverseHead = reverseLL(temp);
-            if(newHead==nullptr)
-            {
-               newHead = reverseHead;
-            }
-            else
-            {
-                if(prevNode)prevNode->next = reverseHead;
-            }
-            prevNode = temp;
-            temp = nextNode;
+        if(head==NULL || head->next==NULL){
+            return head;
         }
-       return newHead? newHead:head;
+
+        int count = 1;
+
+        ListNode *temp = head;
+        ListNode *prev = NULL;
+
+        while(count<=k && temp){
+            ListNode *nextNode = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = nextNode;
+            count++;
+        }
+
+        if(count<=k){
+            
+            temp=prev;
+            prev=NULL;
+
+            while(temp){
+            ListNode *nextNode = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = nextNode;
+            
+             }
+
+             
+
+        }
+
+        else{
+
+        ListNode *oldHead = head;
+        head->next=temp;
+        head=prev;
+
+        oldHead->next = reverseKGroup(temp,k);
+
+        }
+
+        return head;
+
     }
 };
