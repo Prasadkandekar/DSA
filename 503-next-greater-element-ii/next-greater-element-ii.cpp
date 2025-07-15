871a3c7f837bc22ea4b5c38797232f47aa44970b
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2025-07-16 00:01:51 +0530

    Time: 3 ms (75.45%) | Memory: 28.6 MB (38.17%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,15 +4,37 @@ public:
         
         int n = nums.size();
         vector<int>nge(n,-1);
-        for(int i= 0 ;i < n; i++){
-            for(int j = i+1 ; j < i+n ;j++){
-                int idx = j%n;
-                if(nums[idx]> nums[i]){
-                    nge[i]=nums[idx];
-                    break;
-                }
+        stack<int>st;
+        for(int i = 2*n-1 ;i >=0  ;i--){
+            while(!st.empty() && st.top() <= nums[i%n]){
+                st.pop();
             }
+            if(i < n){
+                nge[i] = (st.empty())?-1:st.top();
+            }
+            st.push(nums[i%n]);
         }
         return nge;
     }
 };
+
+
+//Brute force:
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         vector<int>nge(n,-1);
+//         for(int i= 0 ;i < n; i++){
+//             for(int j = i+1 ; j < i+n ;j++){
+//                 int idx = j%n;
+//                 if(nums[idx]> nums[i]){
+//                     nge[i]=nums[idx];
+//                     break;
+//                 }
+//             }
+//         }
+//         return nge;
+//     }
+// };
