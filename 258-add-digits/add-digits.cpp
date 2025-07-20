49819092e38e0051b7bf786d025e74fd306f716f
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2025-07-20 19:09:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.87%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -2,16 +2,27 @@ class Solution {
 
 public:
     int addDigits(int num) {
-        int ans = num;
-        int sum=0;
-        while(ans){
-            int last = ans%10;
-            sum +=last;
-            ans = ans/10;
-        }
-        if(sum >9){
-       sum =  addDigits(sum);
-        }
-         return sum;
+        if(num==0)return 0;
+        return 1+(num-1)%9;
     }
 };
+
+//Recursive solution:
+
+// class Solution {
+
+// public:
+//     int addDigits(int num) {
+//         int ans = num;
+//         int sum=0;
+//         while(ans){
+//             int last = ans%10;
+//             sum +=last;
+//             ans = ans/10;
+//         }
+//         if(sum >9){
+//        sum =  addDigits(sum);
+//         }
+//          return sum;
+//     }
+// };
