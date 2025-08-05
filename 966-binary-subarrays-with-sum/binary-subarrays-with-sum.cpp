3acfe9ef9a13711638bb70a669e46913eb75e697
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2025-08-05 19:25:30 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (59.48%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
-    int atMost(vector<int>&nums ,int goal){
-         if(goal < 0)return 0;
+    int lessThanOrEqualTo(vector<int>&nums,int goal){
+        if(goal < 0)return 0;
         int n = nums.size();
-        int left,right;
-        left = right = 0;
         int sum = 0;
         int cnt = 0;
-
-        while(right < n){
-            sum += nums[right];
-           while(sum > goal){
-            sum -= nums[left];;
-            left = left+1;
-           }
-            cnt = cnt + (right-left+1);
-            right++;
-        }
+        int l = 0;
+        int r = 0;
+        while(r < n ){
+            sum += nums[r];
+            while(sum > goal){
+                sum -=nums[l];
+                l++;
+            }
+            cnt += r-l+1;//lenght;
+            r++;
+        }   
         return cnt;
     }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-       
-       return atMost(nums,goal)-atMost(nums,goal-1);
+        return lessThanOrEqualTo(nums,goal)-lessThanOrEqualTo(nums,goal-1);
     }
 };
-
-
-//Brute Force : 
-// class Solution {
-// public:
-//     int numSubarraysWithSum(vector<int>& nums, int goal) {
-//         int n = nums.size();
-//         int ans = 0;
-//         for(int i = 0 ;i <  n; i++){
-//             int sum = 0;
-//             for(int j = i ;j < n;j++){
-//                 sum += nums[j];
-//                  if(sum==goal)ans++;
-//             }
-           
-//         }
-//         return ans;
-//     }
-// };
