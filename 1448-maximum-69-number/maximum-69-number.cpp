c5657647f95bba5903ac1715c2325c090362f8dd
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2025-08-16 09:51:17 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (71.54%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        vector<int>num69;
-        while(num){
-            int last = num%10;
-            num /= 10;
-            num69.push_back(last);
+       string n = to_string(num);
+       for(auto &s:n){
+        if(s=='6'){
+            s = '9';
+            break;
         }
-        for(int  i = num69.size()-1; i >=0 ;i--){
-            if(num69[i]==6){
-                num69[i]=9;
-                break;
-            }
-        }
-        long long ans = 0;
-        for( int i = num69.size()-1 ; i >=0 ;i--){
-            ans = ans*10+num69[i];
-        }
-        return ans;
+       }
+       return stoi(n);
     }
 };
+
+
+//Brute force 
+// class Solution {
+// public:
+//     int maximum69Number (int num) {
+//         vector<int>num69;
+//         while(num){
+//             int last = num%10;
+//             num /= 10;
+//             num69.push_back(last);
+//         }
+//         for(int  i = num69.size()-1; i >=0 ;i--){
+//             if(num69[i]==6){
+//                 num69[i]=9;
+//                 break;
+//             }
+//         }
+//         long long ans = 0;
+//         for( int i = num69.size()-1 ; i >=0 ;i--){
+//             ans = ans*10+num69[i];
+//         }
+//         return ans;
+//     }
+// };
