4790dace0fa022f0c99d4f13070eff3c54863d1e
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2025-08-16 09:45:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (41.82%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        vector<int>num69;
+        while(num){
+            int last = num%10;
+            num /= 10;
+            num69.push_back(last);
+        }
+        for(int  i = num69.size()-1; i >=0 ;i--){
+            if(num69[i]==6){
+                num69[i]=9;
+                break;
+            }
+        }
+        long long ans = 0;
+        for( int i = num69.size()-1 ; i >=0 ;i--){
+            ans = ans*10+num69[i];
+        }
+        return ans;
+    }
+};
