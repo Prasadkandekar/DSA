05bfbac04a24f330c48008ef698d00f426bc4af5
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-02-15 09:23:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (74.14%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    void dfs(int node,vector<vector<int>>& isConnected,vector<int>& visited){
+        visited[node] = 1;
+
+        // for(auto neighbor : isConnected[node]){
+        //     if(!visited[neighbor]){
+        //         dfs(neighbor,isConnected,visited);
+        //     }
+        // }
+
+        for(int i = 0 ; i< isConnected.size() ;i++){
+
+            if(isConnected[node][i]==1 && visited[i] == 0){
+                dfs(i,isConnected,visited);
+            }
+        }
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int provinces = 0;
+        int n = isConnected.size();
+        vector<int>visited(n,0);
+
+        for(int i = 0 ; i < n ;i++){
+            int node = i;
+            if(!visited[node]){
+                dfs(node,isConnected,visited);
+                provinces++;
+            }
+        }
+
+    
+        return provinces;
+
+    }
+};
