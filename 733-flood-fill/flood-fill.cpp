457fb55bb1b2cf1c29869d5cfc088af274208905
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-02-16 09:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (8.71%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
-    void fill(vector<vector<int>>&image,int y, int x, int color , int newColor){
-        if(y <0 || y >= image.size() || x < 0 || x >= image[y].size() || image[y][x] != color)return;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        
+        if(image.empty())return image;
+        
 
-        image[y][x] = newColor;
-        fill(image,y,x+1,color , newColor);
-        fill(image,y,x-1,color , newColor);
-        fill(image,y-1,x,color , newColor);
-        fill(image,y+1,x,color , newColor);
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        if(image[sr][sc]==newColor)return image;
-   
-    //recursive flood fill
-    fill(image,sr,sc,image[sr][sc],newColor);
-    return image;
+        int m = image.size();
+        int n = image[0].size();
+
+        int drow[] = {1,0,-1,0};
+        int dcol[] = {0,1,0,-1};
+
+        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        q.push({{sr,sc},image[sr][sc]});
+        image[sr][sc] = color;
+        visited[sr][sc] = 1;
+
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int old_color = q.front().second;
+            q.pop();
+            image[row][col] = color;
+
+            for(int i = 0 ; i < 4 ;i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+               
+
+                if(nrow >=0  && nrow < m && ncol >=0  && ncol < n && !visited[nrow][ncol] && image[nrow][ncol] == old_color){
+                    q.push({{nrow,ncol},image[nrow][ncol]});
+                    visited[nrow][ncol] = 1;
+                }
+            }
+        }
+        return image;
     }
 };
