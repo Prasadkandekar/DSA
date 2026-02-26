d0374e3c6e32686b0c439178164dfe952ba17fe1
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-02-26 10:12:52 +0530

    Time: 54 ms (71.16%) | Memory: 21.3 MB (44.04%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        
-        unordered_set<string> st(wordList.begin(),wordList.end());
+        unordered_set<string>st(wordList.begin(),wordList.end());
         queue<pair<string,int>>q;
         q.push({beginWord,1});
         st.erase(beginWord);
@@ -12,23 +11,21 @@ public:
             string word = q.front().first;
             int steps = q.front().second;
             q.pop();
-            if(word == endWord) return steps;
 
-            for(int i = 0 ; i < word.size();i++){
+            if(endWord == word) return steps;
+
+            for(int i = 0 ; i< word.size(); i++){
                 char original = word[i];
-                for(char ch = 'a'; ch <='z'; ch++){
+                for(char ch ='a'; ch <= 'z' ; ch++){
                     word[i] = ch;
                     if(st.find(word) != st.end()){
-                        st.erase(word);
                         q.push({word,steps+1});
+                        st.erase(word);
                     }
                 }
                 word[i] = original;
             }
         }
-
-
         return 0;
     }
 };
-auto init = atexit([](){ofstream("display_runtime.txt") << "1000";});
