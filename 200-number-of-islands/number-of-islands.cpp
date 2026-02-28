5ba8bbbe67bfb3d2c494575eb7efd0b4dcb89c28
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-02-28 12:41:14 +0530

    Time: 23 ms (83.62%) | Memory: 17.3 MB (49.62%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -31,6 +31,27 @@ private:
 
         }
     }
+    void dfs( int row, int col , vector<vector<char>>& grid, vector<vector<int>>& visited){
+        
+        int n = grid.size();
+        int m = grid[0].size();
+
+        if(grid[row][col]=='1'){
+            visited[row][col]=1;
+        }
+
+        int dx[] = {1,0,-1,0};
+        int dy[] = {0,1,0,-1};
+
+        for(int i = 0 ;i < 4 ;i++){
+            int nrow = row + dx[i];
+            int ncol = col + dy[i];
+            if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m && grid[nrow][ncol]=='1' && !visited[nrow][ncol]){
+                dfs(nrow,ncol,grid,visited);
+            }
+        }
+
+    }
 public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
@@ -43,7 +64,8 @@ public:
             for(int j= 0 ; j<m ; j++){
                 if(grid[i][j]=='1' && !visited[i][j]){
                     count++;
-                    bfs(i,j, grid, visited);
+                    // bfs(i,j, grid, visited);
+                     dfs(i,j, grid, visited);
                 }
             }
         }
