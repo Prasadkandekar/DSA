68b477eedd26892b736c1a81ba9183ab97f1c872
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-03-01 09:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (89.61%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,10 +1,11 @@
 class Solution {
 private:
-    void dfs(int row,int col,vector<vector<int>>& image,int color,vector<vector<int>>& visited, int startColor){
-        int n = image.size();
-        int m = image[0].size();
+    void dfs(int row,int col,vector<vector<int>>& image,int color, int startColor){
+       
+         int n = image.size();
+         int m = image[0].size();
         
-         visited[row][col] = 1;
+        if(row  < 0 || row >= n || col < 0 || col >= m || image[row][col] != startColor)return;
          image[row][col] = color;
 
          int drow[] = {1,0,-1,0};
@@ -13,21 +14,16 @@ private:
          for(int i = 0  ; i < 4 ;i++){
             int nrow = row + drow[i];
             int ncol = col + dcol[i];
-            
-
-            if(nrow >=0 && nrow < n && ncol >= 0 && ncol < m && !visited[nrow][ncol] && image[nrow][ncol]==startColor){
-                dfs(nrow,ncol,image,color,visited,image[nrow][ncol]);
+            if(nrow >=0 && nrow < n && ncol >= 0 && ncol < m ){
+                dfs(nrow,ncol,image,color,startColor);
             }
          }
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-
-        vector<vector<int>> visited(n,vector<int>(m,0));
         int startColor = image[sr][sc];
-        dfs(sr,sc,image,color,visited,startColor);
+         if(startColor == color)return image;
+        dfs(sr,sc,image,color,startColor);
         return image;
     }
 };
