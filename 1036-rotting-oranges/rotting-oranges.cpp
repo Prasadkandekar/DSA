b1bd6682e65197e52f897f79870a6da34f914fb7
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-03-01 09:02:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (30.86%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-       int m = grid.size();
-       int n = grid[0].size();
-
+        int n = grid.size();
+        int m = grid[0].size();
+        int time = 0;
         queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>> visited(m,vector<int>(n,0));
-
-        for(int i = 0 ;i < m ;i++){
-            for(int j = 0 ; j < n ; j++){
-                if(grid[i][j] == 2){
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        
+        //multi source bfs
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j]==2 && !visited[i][j]){
                     q.push({{i,j},0});
-                    visited[i][j] = 2;
                 }
             }
         }
 
-         int delta_row[] = {-1,0,+1,0};
-         int delta_col[] = {0,+1,0,-1};
-         int time = 0;
-
+        int drow[] = {1,0,-1,0};
+        int dcol[] = {0,1,0,-1};
         while(!q.empty()){
-               int row = q.front().first.first;
-               int col = q.front().first.second;
-               int t = q.front().second;
-               q.pop();
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int currTime = q.front().second;
+            q.pop();
 
-               time = max(time,t);
+            time = max(time,currTime);
 
-               for(int i = 0 ;i < 4 ;i++){
-                    int nrow = row + delta_row[i];
-                    int ncol = col + delta_col[i];
+            for(int i = 0 ;i < 4 ; i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
 
-                    if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && visited[nrow][ncol] != 2 &&  grid[nrow][ncol]== 1 ){
-                        visited[nrow][ncol] = 2;
-                        q.push({{nrow,ncol},t+1});
-                    }
-               }
+                if(nrow >= 0 && nrow < n  && ncol >= 0 && ncol < m && grid[nrow][ncol]==1 && !visited[nrow][ncol]){
+                     visited[nrow][ncol] = 1;
+                     q.push({{nrow,ncol},currTime+1});
+                }
+            }
+            
         }
 
-        for(int i = 0 ;i < m ;i++){
-            for(int j = 0 ;j < n ;j++){
-                if( visited[i][j] != 2 && grid[i][j]==1)return -1;
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j<m ; j++){
+                if(grid[i][j]==1 && !visited[i][j]){
+                   return -1;
+                }
             }
         }
-        return time;
+
+        return time ;
     }
 };
