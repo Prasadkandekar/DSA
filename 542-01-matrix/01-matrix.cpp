b1630c681b9d8379a1336751038feddd7af50d95
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-03-01 10:39:45 +0530

    Time: 15 ms (71.75%) | Memory: 38.9 MB (20.49%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        if(mat.empty())return mat;
+        int n = mat.size();
+        int m = mat[0].size();
 
-        int m = mat.size();
-        int n = mat[0].size();
-
-        queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>> dist(m,vector<int>(n,0));
-        vector<vector<int>> visited(m,vector<int>(n,0));
-
-        for(int i = 0 ;i < m ;i++){
-            for(int j = 0 ;j < n ;j++){
+        queue<tuple<int,int,int>>q;
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        vector<vector<int>>dist(n,vector<int>(m,0));
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
                 if(mat[i][j]==0){
+                    q.emplace(i,j,0);
                     visited[i][j] = 1;
-                    q.push({{i,j},0});
                 }
             }
         }
-            int drow[4] = {-1,0,1,0};
-            int dcol[4] = {0,-1,0,1};
-            int min_dist = 0;
+
+        int drow[] = {1,0,-1,0};
+        int dcol[] = {0,1,0,-1};
+
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int curr_dist = q.front().second;
+            auto [row,col,curr_dist] = q.front();
             dist[row][col] = curr_dist;
             q.pop();
 
-            for(int i = 0 ;i <  4;i++){
+            for(int i = 0; i < 4 ; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow >=0 && nrow < m && ncol >=0 && ncol < n && !visited[nrow][ncol]){
+                if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m && !visited[nrow][ncol]){
                     visited[nrow][ncol] = 1;
-                    q.push({{nrow,ncol},curr_dist+1});
-                   
+                    q.emplace(nrow,ncol,curr_dist+1);
                 }
             }
+
         }
-    return dist;
+
+        return dist;
     }
 };
