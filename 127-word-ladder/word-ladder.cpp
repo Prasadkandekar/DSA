7bdee5e3c9a654e7785586dc646c31690465a597
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-03-01 12:34:12 +0530

    Time: 57 ms (62.37%) | Memory: 21.2 MB (59.41%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -6,26 +6,27 @@ public:
         q.push({beginWord,1});
         st.erase(beginWord);
 
-
         while(!q.empty()){
             string word = q.front().first;
             int steps = q.front().second;
             q.pop();
 
-            if(endWord == word) return steps;
+            if(endWord== word)return steps;
 
-            for(int i = 0 ; i< word.size(); i++){
+            for(int i = 0 ; i < word.size(); i++){
                 char original = word[i];
-                for(char ch ='a'; ch <= 'z' ; ch++){
+                for(char ch = 'a' ; ch <= 'z' ; ch++){
                     word[i] = ch;
                     if(st.find(word) != st.end()){
-                        q.push({word,steps+1});
+                        q.push({word,steps + 1});
                         st.erase(word);
                     }
                 }
                 word[i] = original;
             }
         }
+
+
         return 0;
     }
 };
