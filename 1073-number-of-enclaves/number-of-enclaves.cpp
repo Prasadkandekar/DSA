c7cfb25d464b1bb7cc8b667c495cf3099cba9db9
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-03-01 11:25:35 +0530

    Time: 13 ms (23.90%) | Memory: 31.7 MB (72.64%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        if(grid.empty())return 0;
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>>visited(m,vector<int>(n,0));
-        int count = 0;
-        queue<pair<int,int>>q;
+        int n = grid.size();
+        int m = grid[0].size();
 
-        for(int i= 0 ;i< m;i++){
-            for(int j = 0 ; j < n; j++){
-                if((i==0 || j==0 || i == m-1 || j==n-1) && grid[i][j]==1){
-                    visited[i][j] = 1;
-                    q.push({i,j});
+        int enclaves = 0;
+        queue<pair<int,int>>q;
+        vector<vector<int>>visited(n,vector<int>(m,0));
+
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ; j<m ; j++){
+                if(i==0 || i==n-1 || j==0 || j==m-1){
+                    if(!visited[i][j] && grid[i][j]==1){
+                        q.push({i,j});
+                        visited[i][j] = 1;                    }
                 }
             }
         }
 
-        int drow[] = {1,0,-1,0};
-        int dcol[] = {0,-1,0,1};
+        int drow[]  = {1,0,-1,0};
+        int dcol[] = {0,1,0,-1};
 
         while(!q.empty()){
-            int row =  q.front().first;
+            int row = q.front().first;
             int col = q.front().second;
             q.pop();
 
-            for(int i= 0; i < 4 ;i++){
+            for(int i = 0 ; i<4 ; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow >=0 && nrow < m && ncol >=0 && ncol < n && !visited[nrow][ncol] && grid[nrow][ncol]==1){
-                    visited[nrow][ncol] = 1;
+                if(nrow >=0 && nrow < n && ncol >= 0 && ncol < m && !visited[nrow][ncol] && grid[nrow][ncol]==1){
                     q.push({nrow,ncol});
+                    visited[nrow][ncol] = 1;
                 }
-
             }
         }
-         for(int i=0 ; i < m ;i++){
-                for(int j=0 ; j < n ; j++){
-                    if(!visited[i][j] && grid[i][j]==1){
-                        count++;
-                    }
-                }
-            }
 
-    return count;
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ; j<m ; j++){
+                   if(!visited[i][j] && grid[i][j]==1){
+                        enclaves++;
+                    }   
+            }
+        }
+        
+        return enclaves;
     }
 };
