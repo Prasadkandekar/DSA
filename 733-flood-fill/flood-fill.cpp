4002b6cbc82da2417f4c14f78c9e624fa9f942c5
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-03-13 11:02:19 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (74.50%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,29 +1,33 @@
 class Solution {
 private:
-    void dfs(int row,int col,vector<vector<int>>& image,int color, int startColor){
-       
-         int n = image.size();
-         int m = image[0].size();
+    void dfs(vector<vector<int>>& image, int r , int c , int color ,int startColor){
+        int n = image.size();
+        int m = image[0].size();
+
+        if(r < 0 || r >= n || c < 0 || c >= m)return;
+        if(image[r][c] !=startColor)return;
+
+        image[r][c]  = color;
+
+     
+        dfs(image,r+1,c,color,startColor);
+        dfs(image,r,c+1,color,startColor);
+        dfs(image,r-1,c,color,startColor);
+        dfs(image,r,c-1,color,startColor);
         
-        if(row  < 0 || row >= n || col < 0 || col >= m || image[row][col] != startColor)return;
-         image[row][col] = color;
+        
+        
+       
 
-         int drow[] = {1,0,-1,0};
-         int dcol[] = {0,1,0,-1};
-
-         for(int i = 0  ; i < 4 ;i++){
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-            if(nrow >=0 && nrow < n && ncol >= 0 && ncol < m ){
-                dfs(nrow,ncol,image,color,startColor);
-            }
-         }
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+         int n = image.size();
+         int m = image[0].size();
+
         int startColor = image[sr][sc];
-         if(startColor == color)return image;
-        dfs(sr,sc,image,color,startColor);
-        return image;
+        if(startColor == color)return image;
+        dfs(image,sr,sc,color,startColor);
+         return image;
     }
 };
