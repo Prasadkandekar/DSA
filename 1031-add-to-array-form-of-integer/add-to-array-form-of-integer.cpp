aa548674fdbbb76ae9788876e4001df39128b47f
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-03-29 13:44:07 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (48.85%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
@@ -1,43 +1,19 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        int n = num.size();
-      
-        string str = to_string(k);
-        cout << str <<endl;
-        int m= str.size();
-        cout << "m,n : " << m << " "<< n<<endl;
-        int l = max(n,m)+1;
-        vector<int> res(l,0);
-
-        int carry =0;
-        int i = n-1;
-        int j = m-1;
-        int r = l-1;
-        while(i >= 0 || j >= 0 || carry){
-            int ival = (i >= 0) ? num[i] : 0;
-            i--;
-            int jval = (j >= 0)?  str[j]-'0' : 0;
-            j--;
-
-            int sum = ival + jval + carry;
-            res[r] = sum%10;
-            carry = sum/10;
-            r--;
-        }
-
         vector<int>ans;
-        bool isLeadZero = true;
-        for(int i = 0 ;i < l ; i++){
-            if(res[i]==0 && isLeadZero){continue;}
-            else{
-                isLeadZero = false;
-                ans.push_back(res[i]);
-            }
+        int i = num.size()-1;
+        while(i >= 0 || k){
+            if(i >= 0){
+                k += num[i];
+                i--;
 
+            }
+                ans.push_back(k%10);
+                k = k/10;
             
         }
-
-    return ans;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
