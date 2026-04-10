6c3d2bcd061af9dd776d61982ea1faf955979128
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-04-10 20:04:46 +0530

    Time: 8 ms (52.11%) | Memory: 15.8 MB (17.41%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,42 +1,55 @@
 class Solution {
-public:
-    int swimInWater(vector<vector<int>>& grid) {
-         int n = grid.size();
-        int m = grid[0].size();
+private:
+    bool canReach(vector<vector<int>>&grid , int t){
+        int n = grid.size();
 
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>>pq;
-        vector<vector<int>>dist(n,vector<int>(m,1e9));
-        
-        pq.push({grid[0][0],{0,0}});
-        dist[0][0] = 0;
+        vector<vector<int>>visited(n,vector<int>(n,0));
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        visited[0][0] = 1;
+        if(grid[0][0] > t)return false;
 
-        int drow[] = {1,0,-1,0};
-        int dcol[] = {0,1,0,-1};
+        int drow[] = {1,-1,0,0};
+        int dcol[] = {0,0,1,-1};
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
 
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
-            int diff = it.first;
-            int row = it.second.first;
-            int col = it.second.second;
+            if(row==n-1 && col ==n-1)return true;
 
-            if(row == n-1 && col == m-1 && dist[row][col] != 1e9 )return diff;
-            for(int i = 0 ;i < 4 ;i++){
+            for(int i = 0 ;i < 4 ; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
-
-                if(nrow >= 0  && nrow < n && ncol >= 0 && ncol < m ){
-
-                    int newTime = max(grid[nrow][ncol],diff);
-
-                    if(newTime < dist[nrow][ncol]){
-                        dist[nrow][ncol] = newTime;
-                        pq.push({newTime,{nrow,ncol}});
-                    }
+                
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n && !visited[nrow][ncol] && grid[nrow][ncol] <= t ){
+                    visited[nrow][ncol] = 1;
+                    q.push({nrow,ncol});
                 }
             }
-
         }
-       return dist[n-1][m-1];
+    return false;
+    }
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        //Binary Search + BFS approach
+        int n =grid.size();
+        int left = grid[0][0];
+        int right = n*n-1;
+        int ans = right;
+
+        while(left <= right){
+            int mid = (right + left)/2;
+
+            if(canReach(grid,mid)){
+                ans = mid;
+                right = mid-1;
+
+            }else{
+                left  = mid+1;
+            }
+        }
+
+        return ans;
     }
 };
