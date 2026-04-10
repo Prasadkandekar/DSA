2077d55fafd7ea7411e2260a90247a2b09764d6f
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-04-10 19:07:57 +0530

    Time: 188 ms (64.33%) | Memory: 109.6 MB (76.57%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,53 +1,58 @@
-class DisjointSet{
-    public: 
-        vector<int>parent,size;
-        DisjointSet(int n){
-            size.resize(n+1,1);
-            parent.resize(n+1);
-            for(int i = 1 ;i <= n ; i++){
-                parent[i] = i;
-            }
-        };
-        int findParent(int node){
-            if(node == parent[node])return node;
-
-            return parent[node] = findParent(parent[node]);
+class DisjointSet {
+public:
+    vector<int> parent, size;
+    DisjointSet(int n) {
+        size.resize(n + 1, 1);
+        parent.resize(n + 1);
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
         }
-        void unionBySize(int u , int v){
-            int ulp_u = findParent(u);
-            int ulp_v = findParent(v);
-            
-            if(ulp_u == ulp_v) return;
+    };
+    int findParent(int node) {
+        if (node == parent[node])
+            return node;
 
-            if(size[ulp_u] < size[ulp_v]){
-                parent[ulp_u] = ulp_v;
-                size[ulp_v] += size[ulp_u];
-            }else{
-                parent[ulp_v] = ulp_u;
-                size[ulp_u] += size[ulp_v];
-            }
+        return parent[node] = findParent(parent[node]);
+    }
+    void unionBySize(int u, int v) {
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+
+        if (ulp_u == ulp_v)
+            return;
+
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
         }
+    }
 };
 
 class Solution {
 public:
     int largestIsland(vector<vector<int>>& grid) {
         int n = grid.size();
-        DisjointSet ds(n*n );
+        DisjointSet ds(n * n);
 
-        //step1 : connects ones in the grid
-        for(int i =  0 ;i < n ;i++){
-            for(int j = 0;j < n ;j++){
+
+        //step1 : connect cells containing ones
+        for(int i = 0 ;i < n ;i++){
+            for(int j = 0 ; j < n ;j++){
                 if(grid[i][j]==0)continue;
 
-                int drow[4] = {1,-1,0,0};
-                int dcol[4] = {0,0,1,-1};
+                int drow[4] = {0,0,1,-1};
+                int dcol[4] = {1,-1,0,0};
                 int row = i;
                 int col = j;
+
                 for(int k = 0 ;k < 4 ; k++){
                     int nrow = row + drow[k];
                     int ncol = col + dcol[k];
 
+                    //check valid
                     if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n && grid[nrow][ncol]==1){
                         int currNode = row*n + col;
                         int adjNode = nrow*n + ncol;
@@ -57,41 +62,50 @@ public:
             }
         }
 
-        //step 2 : try to convert 0 to 1 and find largest Island
+        //step 2 : try replacing 0 with ones and find larger island
+       
         int maxIsland = 0;
-        for(int i = 0 ;i  < n ; i++){
-            for(int j = 0 ;j < n ; j++){
+        for(int i = 0 ;i < n ;i++){
+            for(int j = 0 ; j < n ;j++){
                 if(grid[i][j]==1)continue;
 
+                 set<int>components;
 
-                int drow[4] = {1,-1,0,0};
-                int dcol[4] = {0,0,1,-1};
+                int drow[4] = {0,0,1,-1};
+                int dcol[4] = {1,-1,0,0};
                 int row = i;
                 int col = j;
-                set<int>components;
+
                 for(int k = 0 ;k < 4 ; k++){
                     int nrow = row + drow[k];
                     int ncol = col + dcol[k];
 
-                    if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n ){
-                        if(grid[nrow][ncol] == 1){
-                                components.insert(ds.findParent(nrow*n+ncol));
+                    //check valid
+                    if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n){
+                        if(grid[nrow][ncol]==1){
+                            int currNode = row*n + col;
+                            int adjNode = nrow*n + ncol;
+                            components.insert(ds.findParent(adjNode));
+
                         }
-                        
                     }
                 }
                 int componentSize = 0;
-                for(auto it : components){
-                    componentSize += ds.size[it];
-                }
-            maxIsland = max(maxIsland,componentSize + 1);
+                for( auto it : components){
+                componentSize += ds.size[ds.findParent(it)];
+                maxIsland = max(maxIsland,componentSize + 1);
+            }
             }
         }
+            
 
-        //what if thre are all ones then
-        for(int cellNo = 0 ; cellNo < n*n ; cellNo++){
-            maxIsland = max(maxIsland, ds.size[ds.findParent(cellNo)] );
-        }
+            
+
+
+            //last , what if there are all ones:then :
+            for(int cellNo = 0 ; cellNo < n*n ; cellNo++){
+                maxIsland = max(maxIsland, ds.size[ds.findParent(cellNo)]);
+            }
 
         return maxIsland;
     }
