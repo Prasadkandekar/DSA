cd5373487a4e8747b528c49d8026e3f95e236cdb
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-04-24 01:25:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (88.08%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();vector<int>dp(n,0);
-        dp[0] = nums[0]; 
-        if(n > 1)
-        dp[1] = max(nums[0],nums[1]);
-        for(int i = 2 ; i < n ;i++)dp[i] = max(dp[i-1],dp[i-2] + nums[i]);
-        return dp[n-1];
+        int n = nums.size();
+        int prev1 = 0;
+        int prev2 = 0;
+
+        for(int i = 0 ;i < n;i++){
+            
+            int curr_i = max(prev1,prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr_i;
+        }
+
+        return prev1;    
     }
 };
 
+// dp --> tabulation (bottom up approach)
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();vector<int>dp(n,0);
+//         dp[0] = nums[0]; 
+//         if(n > 1)
+//         dp[1] = max(nums[0],nums[1]);
+//         for(int i = 2 ; i < n ;i++)dp[i] = max(dp[i-1],dp[i-2] + nums[i]);
+//         return dp[n-1];
+//     }
+// };
 // dp --> memozation : passed
 // class Solution {
 // private:
