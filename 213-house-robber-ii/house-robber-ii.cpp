5bbd04e55462ebd8081a26805f64eee71f23b478
Prasad Trimbak Kandekar <145256223+Prasadkandekar@users.noreply.github.com>
2026-04-25 19:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (50.83%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,29 +1,26 @@
 class Solution {
-    int solve2(int idx , vector<int>&nums,vector<int>& dp){
-        if(idx <= 0)return 0;
-        if(idx == 1)return nums[1];
-        if(idx == 2)return  max(nums[1],nums[2]);
-        if(dp[idx] != -1)return dp[idx];
-
-        return dp[idx] = max(solve2(idx-1,nums,dp),solve2(idx-2,nums,dp) + nums[idx]);
-    }
-    
-    int solve1(int idx , vector<int>& nums, vector<int>&dp){
-        if(idx == 0)return nums[0];
-        if(idx == 1)return max(nums[0],nums[1]);
-          if(dp[idx] != -1)return dp[idx];
-        return dp[idx] = max(solve1(idx-1,nums,dp), solve1(idx-2,nums,dp) + nums[idx]);
-    }
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size()-1;
-        if(n==0)return nums[0];
-        if(n==1)return max(nums[0],nums[1]);
-        vector<int>dp1(n+1,-1);
-        int rob1 = solve1(n-1,nums,dp1);
-         vector<int>dp2(n+1,-1);
-        int rob2 = solve2(n,nums,dp2);
+        // trying tabulation by own;
+        int n = nums.size();
+        if(n==1)return nums[0];
+        if(n==2)return max(nums[0],nums[1]);
+        vector<int>dp1(n+1,0);
+       
+        dp1[0] = nums[0];
+        dp1[1] = max(nums[0],nums[1]);
+        for(int i = 2; i < n-1 ;i++){
+         dp1[i] = max(dp1[i-1],dp1[i-2] + nums[i]);
+        }
 
-        return max(rob1, rob2);
+        vector<int>dp2(n+1,0);
+    
+        dp2[1] = nums[1];
+        dp2[2] = max(nums[1],nums[2]);
+        for(int i = 3; i < n;i++){
+         dp2[i] = max(dp2[i-1],dp2[i-2] + nums[i]);
+        }
+
+    return max(dp1[n-2],dp2[n-1]);
     }
 };
